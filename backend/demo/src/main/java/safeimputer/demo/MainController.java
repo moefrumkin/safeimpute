e0627e1d68774aef2f3a4a6a6cf5ef4308010383
AtticusColwell <atticusscott@icloud.com>
2023-09-16 02:07:38 -0400

    changes

--- backend/demo/src/main/java/safeimputer/demo/MainController.java
+++ backend/demo/src/main/java/safeimputer/demo/MainController.java
@@ -1,6 +1,12 @@
 package safeimputer.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +15,23 @@ public class MainController {
     @GetMapping("/yeo")
     public String sayYeo(){
         return "YEOOO";
+    }
+
+    @GetMapping("/NumberOfPoints")
+    public int getNumberOfPoints() {
+        return 1; // this will be subbed out for number of points for graph
+    }
+
+    @GetMapping("/Points")
+    public HashMap<Double, Double> getPoints() {
+        return new HashMap<Double, Double>();
+    }
+
+    @PostMapping("/parametersUpload") 
+    public ResponseEntity<String> handleRequest(@RequestBody Map<String, String> requestPayload) {
+        String parameter1 = requestPayload.get("parameter1");
+        String parameter2 = requestPayload.get("parameter2");
+
+        return ResponseEntity.ok("Parameters received: " + parameter1 + ", " + parameter2); // can include more parameters
     }
 }
