b43567863060efd029bb80807bd542c2318eb123
blackburn <matthewalexanderb@gmail.com>
2023-09-16 02:08:58 -0400

    changes

--- backend/demo/src/main/java/safeimputer/demo/MainController.java
+++ backend/demo/src/main/java/safeimputer/demo/MainController.java
@@ -1,13 +1,17 @@
 package safeimputer.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class MainController {
+
     
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/yeo")
-    public String sayYeo(){
+    public Object sayYeo(){
         return "YEOOO";
     }
 }
