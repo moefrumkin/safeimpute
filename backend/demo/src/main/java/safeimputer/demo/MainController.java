629f41ab00b3d53182d2ea7e23bf450b5be26b5e
Atticus Colwell <atticuscolwell@sylvias-mbp-4.acasmart.jh.edu>
2023-09-15 22:45:38 -0400

    spring boot basic set up

---
+++ backend/demo/src/main/java/safeimputer/demo/MainController.java
@@ -0,0 +1,11 @@
+package main.java.safeimputer.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class MainController {
+    
+    public String sayYeo(){
+        return "YEOOO";
+    }
+}
---
+++ backend/demo/src/main/java/safeimputer/demo/SafeimputeApplication.java
@@ -0,0 +1,13 @@
+package safeimputer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeimputeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeimputeApplication.class, args);
+	}
+
+}
