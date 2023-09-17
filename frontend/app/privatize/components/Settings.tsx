76792756febe7fb98e8466ff579a98dddc000992
blackburn <matthewalexanderb@gmail.com>
2023-09-16 23:58:50 -0400

    added type checking and validity

--- frontend/app/privatize/components/Settings.tsx
+++ frontend/app/privatize/components/Settings.tsx
@@ -8,6 +8,13 @@ import {
 
 
 
+type InputData = {
+  minPrivacy: number | null;
+  maxPrivacy: number | null;
+  numSteps: number | null;
+}
+
+
 type SettingsProps = {
   buttonHandler: (n: string) => void;
 };
@@ -20,8 +27,45 @@ const Settings = ({ buttonHandler }: SettingsProps) => {
   const maxPrivacyRef = useRef<HTMLInputElement>(null);
   const numStepsRef = useRef<HTMLInputElement>(null);
 
-  const validateInput = (data: Object) => {
-    return (1)
+  const [inputValidity, setInputValidity] = useState<{ [key: string]: boolean }>({
+    minPrivacy: true,
+    maxPrivacy: true,
+    numSteps: true,
+  });
+  
+
+  const validateInput = (data: InputData) => {
+    let isValid = true;
+
+    let newInputValidity = { ...inputValidity };
+
+    if(data.minPrivacy === null || isNaN(data.minPrivacy) || data.minPrivacy < 0) {
+      console.error('minPrivacy is not a valid number');
+      newInputValidity.minPrivacy = false; 
+    } else {
+      newInputValidity.minPrivacy = true; 
+    }
+    
+    if(data.maxPrivacy === null || isNaN(data.maxPrivacy) || data.maxPrivacy < 0) {
+      console.error('maxPrivacy is not a valid number');
+      newInputValidity.maxPrivacy = false; 
+    } else {
+      newInputValidity.maxPrivacy = true; 
+    }
+    
+    if(data.numSteps === null || isNaN(data.numSteps) || data.numSteps < 0) {
+      console.error('numSteps is not a valid number');
+      newInputValidity.numSteps = true; 
+    } else {
+      newInputValidity.numSteps = true; 
+    }
+  
+    if (isValid) {
+
+    }
+
+    setInputValidity(newInputValidity);
+    return isValid
   }
 
   const invalidDataHandler = () => {
@@ -31,9 +75,9 @@ const Settings = ({ buttonHandler }: SettingsProps) => {
   const handleClick = async () => {
 
     const inputData = {
-      "minPrivacy": minPrivacyRef.current.value,
-      "maxPrivacy": maxPrivacyRef.current.value,
-      "numSteps": maxPrivacyRef.current.value,
+      minPrivacy: minPrivacyRef.current ? parseInt(minPrivacyRef.current.value) : null,
+      maxPrivacy: maxPrivacyRef.current ? parseInt(maxPrivacyRef.current.value) : null,
+      numSteps: maxPrivacyRef.current ? parseInt(maxPrivacyRef.current.value) : null,
     }
 
     const validated = validateInput(inputData)
@@ -62,17 +106,8 @@ const Settings = ({ buttonHandler }: SettingsProps) => {
     } else {
       invalidDataHandler()
     }
-  }
-
-  const [toggle, setToggle] = useState(false)
-
-  const toggleHandler = () => {
-    if(toggle) {
-      setToggle(false)
-    } else {
-      setToggle(true)
     }
-  }
+  
 
   return (
     
@@ -88,23 +123,27 @@ const Settings = ({ buttonHandler }: SettingsProps) => {
           <div className="flex">
             <p className="w-1/3 left-0">Min. Privacy</p>
             <input ref={minPrivacyRef} className="ml-6 p-1 rounded-sm bg-white/10 border w-1/2 left-0" ></input>
-            {toggle && (
+            {!inputValidity.minPrivacy && (
             <div className="z-50  rounded-md bg-black/20 text-popover-foreground shadow-md outline-none animate-in  fade-in-0  zoom-in-80 slide-in-from-left-2 absolute ml-72  p-1 text-sm font-light text-red-800 border-red-800 border">please enter a valid value</div>
             )}
           </div>
           <div className="flex">
             <p className="w-1/3 left-0">Max. Privacy</p>
             <input ref={maxPrivacyRef} className="ml-6 p-1 rounded-sm bg-white/10 border w-1/2 left-0"></input>
+            {!inputValidity.maxPrivacy && (
+            <div className="z-50  rounded-md bg-black/20 text-popover-foreground shadow-md outline-none animate-in  fade-in-0  zoom-in-80 slide-in-from-left-2 absolute ml-72  p-1 text-sm font-light text-red-800 border-red-800 border">please enter a valid value</div>
+            )}
           </div>
           <div className="flex">
             <p className="w-1/3 left-0">Num Steps</p>
             <input ref={numStepsRef} className="ml-6 p-1 rounded-sm bg-white/10 border w-1/2 left-0 "></input>
+            {!inputValidity.numSteps && (
+            <div className="z-50  rounded-md bg-black/20 text-popover-foreground shadow-md outline-none animate-in  fade-in-0  zoom-in-80 slide-in-from-left-2 absolute ml-72  p-1 text-sm font-light text-red-800 border-red-800 border">please enter a valid value</div>
+            )}
           </div>
-          <button className="left-0 border border-black/20 bg-black/20 hover:border-white/50 p-2 rounded-sm transition duration-200" onClick={handleClick}>confirm</button>
+          <button className="left-0 border bg-black/20 border-white/50 p-2 rounded-sm transition duration-200 hover:bg-black/30" onClick={handleClick}>confirm</button>
 
         </div>
-        <button onClick={toggleHandler}>test</button>
-
       </div>
       <div>
       </div>
